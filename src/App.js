3008a0b477eb3b5fe854307d8a36f0286752fecc
Nyaco <fairyhappy125@kakao.com>
2021-01-03 14:01:38 +0900

    Merge branch 'master' of https://github.com/nyaco0125/react-study into master

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React from "react";
 
-function Movie({fav_movie}) {
-  return <h1>I love {fav_movie}</h1>
+function Movie({name, time}) {
+  return <div><h1>I love {name}</h1>
+  <h2>runtime: {time}</h2></div>;
 }
+
+const movieILike = [
+  {
+    name: 'Avengers',
+    runtime: '3hr'
+  },
+  {
+    name: 'greatest showman',
+    runtime: '2hr'
+  }
+]
 
 function App() {
   return <div>
-  <h1>Hello!</h1>
-  <Movie fav_movie='Nyaco'/>
-  <Movie fav_movie='Greatest Showman'/>
-  <Movie fav_movie='Mickey Mouse'/>
-  <Movie fav_movie='#살아있다'/>
-  <Movie fav_movie='Avengers'/>
-  <Movie fav_movie='Elf'/>
+  {movieILike.map(movie => (
+    <Movie name={movie.name} time={movie.runtime}/>
+    ))}
   </div>;
 }
 
